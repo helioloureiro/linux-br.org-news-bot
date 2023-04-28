f62a1283061316a44384a0da1755a90c3b30629f
Helio Loureiro <helio.loureiro@ericsson.com>
2023-04-28 15:44:50 +0200

    Improved list of interest.
     - Also fixed the script to add new terms.

--- adding-term-to-interest-list.py
+++ adding-term-to-interest-list.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python3
+import re
+import sys
 
 INTERESTS_LIST_FILE = "interests.list"
 
@@ -7,13 +9,19 @@ new_term = input('Enter the new term: ')
 if len(new_term) < 1:
     raise Exception("Too short term to be added")
 
-current_list = []
+current_list = [new_term]
+print(f'Reading data from {INTERESTS_LIST_FILE}')
 with open(INTERESTS_LIST_FILE) as src:
     for line in src.readlines():
+        if re.search(new_term, line):
+            print(f'The term "{new_term}" is already in file "{INTERESTS_LIST_FILE}"')
+            sys.exit(1)
         current_list.append(line.rstrip())
 
+
+output = "\n".join(sorted(current_list))
+print(f'Writting data {INTERESTS_LIST_FILE}')
 with open(INTERESTS_LIST_FILE, "w") as dst:
-    output = "\n".join(sorted(current_list))
     dst.write(output)
 
 print(f"Term \"{new_term}\" added")
