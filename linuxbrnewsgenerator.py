29aa2451e151392ad1fb44917143420330777eed
Helio Loureiro <helio.loureiro@ericsson.com>
2024-01-12 18:37:10 +0100

    Clean coding
     - better modularization in separated methods

--- linuxbrnewsgenerator.py
+++ linuxbrnewsgenerator.py
@@ -8,6 +8,7 @@ import logging
 import time
 import mimetypes
 import sys
+import configparser
 from heapq import nlargest
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
@@ -43,28 +44,34 @@ scriptName = os.path.basename(__file__)
 logger = logging.getLogger(scriptName)
 logger.setLevel('DEBUG')
 
-with open(INTERESTED_TERMS_FILE) as src:
-    for line in src.readlines():
-        INTERESTED_TERMS.append(line.rstrip())
 
-def getHtmlContent(link : str) -> str:
-    response = requests.get(link)
-    html_content = response.text
+def get_interested_terms():
+    'to populated to interested terms'
+    global INTERESTED_TERMS
+    with open(INTERESTED_TERMS_FILE, encoding="utf-8") as src:
+        for line in src.readlines():
+            INTERESTED_TERMS.append(line.rstrip())
+
+def getHtmlContent(link : str) -> str: # pylint: disable=C0103
+    'To fetch html content and return the text'
+    response = requests.get(link, timeout=10)
     return response.text
 
-def getImageExtension(image_line: str) -> str:
-    imgType =  mimetypes.guess_type(image_line)[0]
+def getImageExtension(image_line: str) -> str: # pylint: disable=C0103
+    'To figure out the image extension name'
+    imgType =  mimetypes.guess_type(image_line)[0] # pylint: disable=C0103
     if imgType == "svg+xml":
-        imgType = "svg"
+        imgType = "svg" # pylint: disable=C0103
     return imgType
 
-def getImage(link : str):
-    logger.debug('getimage() link: ' + link)
+def getImage(link : str): # pylint: disable=C0103
+    'To fetch an image from some url'
+    logger.debug('getimage() link: %s', link)
     extension = getImageExtension(link)
     if extension is None:
         logger.debug('getImage(): %s is None', link)
         return None
-    response = requests.get(link)
+    response = requests.get(link, timeout=10)
 
     save_extension = extension.split("/")[1]
     logger.debug('getimage() suffix: %s', save_extension)
@@ -75,32 +82,35 @@ def getImage(link : str):
     return image_file[1]
 
 def prettyprint(data):
-    if type(data) == dict():
+    'Just nice json formmating and printing'
+    if isinstance(data, dict):
         print(json.dumps(data, indent=4))
-    elif type(data) == list():
+    elif isinstance(data, list):
         for e in data:
             prettyprint(e)
     else:
         print(data)
 
-def applyTextCorrections(text):
+def applyTextCorrections(text): # pylint: disable=C0103
+    'To remove over translations'
     for term, replacement in CORRECTIONS.items():
         if re.search(term, text):
             text = re.sub(term, replacement, text)
     return text
 
 class NewsBot:
+    'Class to control bot behavior'
     def __init__(self, config=None):
         if config is None:
-           raise Exception('Missing --config')
+            raise Exception('Missing --config') # pylint: disable=W0719
 
-        self.configFile = config
+        self.configFile = config # pylint: disable=C0103
 
         self.articles = []
         self.readConfiguration()
 
-    def readConfiguration(self):
-        import configparser
+    def readConfiguration(self): # pylint: disable=C0103
+        'to read configuration in configparser format'
         cfg = configparser.ConfigParser()
         cfg.read(self.configFile)
         self.wordpress = {
@@ -108,26 +118,16 @@ class NewsBot:
             'token' : cfg.get('WORDPRESS', 'TOKEN')
         }
 
-    def getHackerNews(self) -> list:
-        '''
-        get the news from rss and return as dict
-        '''
-        fp = feedparser.parse(HACKERNEWS_FEED)
-
-        for e in fp['entries']:
-            self.articles.append({
-                'title' : e['title'],
-                'link' : e['link'],
-            })
-
-    def getSiteRSSTitles(self) -> list:
+    def getSiteRSSTitles(self) -> list: # pylint: disable=C0103
+        'Fetch the RSS from the site hackernews'
         fp = feedparser.parse(self.wordpress['site'] + '/feed/')
         titles = list()
         for e in fp['entries']:
             titles.append(e['title'])
         return titles
 
-    def isTopicOfInterest(self, text : str) -> bool:
+    def isTopicOfInterest(self, text : str) -> bool: # pylint: disable=C0103
+        'Check whether a text is in the interesting word list or not'
         score = 0
         words_of_interest = []
         for word in INTERESTED_TERMS:
@@ -141,104 +141,13 @@ class NewsBot:
             elif re.search("^" + word.lower() + "[ \.,]", text.lower()):
                 score += 1
                 words_of_interest.append(word)
-        logger.debug(f'"{text}" [SCORE: {score}]')
-        
+        logger.debug('"%s" [SCORE: %s]', text, score)
+
         if score == 0:
             return False
         all_words = ', '.join(words_of_interest)
-        logger.info(f"Ranking: [{all_words}] {score}")
+        logger.info("Ranking: [%s] %s", all_words, score)
         return True
-
-    def getArticles(self) -> list: #pylint: disable=C0103
-        '''
-        Open each news from the list, fetch the data and try to generate a summary.
-        If succeed, then add to the list.
-        '''
-        articles = list()
-        for article in self.articles:
-            if not self.isTopicOfInterest(article['title']):
-                logger.info('Not related to something we might like, so we skip: %s', article['title'])
-                continue
-            logger.info('Interested article: %s', article['title'])
-            try:
-                html_content = getHtmlContent(article['link'])
-            except:
-                continue
-            soup = BeautifulSoup(html_content, "html.parser")
-
-            article_text = ""
-            for element in soup.select("article p"):
-                article_text += "\n" + element.text
-
-            try:
-                sentences = sent_tokenize(article_text)
-            except LookupError:
-                logger.info("initializing nltk")
-                nltk.download('punkt')
-                nltk.download('stopwords')
-
-                # trying again
-                sentences = sent_tokenize(article_text)
-
-            stop_words = set(stopwords.words("english"))
-            word_frequencies = FreqDist()
-            for word in nltk.word_tokenize(article_text):
-                if word.lower() not in stop_words:
-                    word_frequencies[word.lower()] += 1
-
-            sentence_scores = {}
-            for sentence in sentences:
-                for word in nltk.word_tokenize(sentence.lower()):
-                    if word in word_frequencies.keys():
-                        if len(sentence.split(" ")) < 30:
-                            if sentence not in sentence_scores.keys():
-                                sentence_scores[sentence] = word_frequencies[word]
-                            else:
-                                sentence_scores[sentence] += word_frequencies[word]
-
-            summary_sentences = nlargest(
-                5, sentence_scores, key=sentence_scores.get)
-            summary = " ".join(summary_sentences)
-
-            if len(summary) < 5:
-                logger.debug("Too short summary for: %s", summary)
-                # summary too short, so skip to the next
-                continue
-
-            img_tags = soup.find_all('img')
-            img_url = None
-
-            try:
-                img_url = img_tags[0]['src']
-            except (KeyError, IndexError):
-                tags_size = 0
-                if img_tags is not None:
-                    tags_size = len(img_tags)
-                logger.debug("Failed to find image for: %s - tags found: %d", summary, tags_size)
-
-            logger.info('translating: %s', article['title'])
-            try:
-                translated_summary = translator.translate(summary, src='en', dest='pt')
-            except TypeError:
-                # failed to translate, try the next one
-                continue
-            if len(translated_summary.text) < 5:
-                logger.info(' * failed...')
-                continue
-            translated_title = translator.translate(
-                article['title'], src='en', dest='pt')
-
-            content = applyTextCorrections(translated_summary.text)
-            content += "\n\nFonte: <a href=\"" + article['link']
-            content += "\">" + article['link'] + "</a>"
-
-            articles.append({
-                'title': applyTextCorrections(translated_title.text),
-                'content': content,
-                'link': article['link'],
-                'image': img_url
-            })
-        return articles
 
     def generateAlias(self, line : str) -> str: #pylint: disable=C0103
         '''
@@ -292,7 +201,7 @@ class NewsBot:
         else:
             logger.warning("Failed to post picture: " +  #pylint: disable=W1201
                            "%s, path: %s, status_code: %d",
-                (image_link, image_path, media_response.status_code)
+                image_link, image_path, media_response.status_code
             )
         logger.debug('removing image: %s', image_path)
         os.unlink(image_path)
@@ -356,7 +265,6 @@ class NewsBot:
             logger.debug(' * status code: %s', str(resp.status_code))
             #print(' * resp text:', resp.text)
 
-
     def run(self):
         '''
         Simple bot starting point.
@@ -369,13 +277,163 @@ class NewsBot:
 
         self.publishWordPress()
 
+    def getHackerNews(self) -> list: # pylint: disable=C0103
+        '''
+        get the news from rss and return as dict
+        '''
+        fp = feedparser.parse(HACKERNEWS_FEED)
+
+        for e in fp['entries']:
+            self.articles.append({
+                'title' : e['title'],
+                'link' : e['link'],
+            })
+
+    def getArticles(self) -> list: # pylint: disable=C0103
+        '''
+        Open each news from the list, fetch the data and try to generate a summary.
+        If succeed, then add to the list.
+        '''
+        articles = list()
+        for article in self.articles:
+            title = article['title']
+            if not self.isTopicOfInterest(title):
+                logger.info('Not related to something we might like, so we skip: %s', title)
+                continue
+            logger.info('Interested article: %s', title)
+
+            link = article['link']
+            article_text, image_tags = self.get_article_content_and_image(link)
+            summary = self.generate_summary(article_text)
+
+            if self.is_summary_too_short(summary):
+            # if len(summary) < 5:
+                logger.info("Too short summary for: %s (DISCARDED)", title)
+                # summary too short, so skip to the next
+                continue
+
+            image_url = self.get_image_url_from_tag(title, image_tags)
+
+            if image_url is None:
+                logger.warning("Discarding [%s] because of the missed image", title)
+                continue
+
+            logger.info('translating: %s', title)
+            translated_summary = self.translate_article(summary)
+
+            if len(translated_summary.text) < 5:
+                logger.error('failed to translate [%s]', title)
+                continue
+
+            translated_title = self.translate_article(title)
+
+            content = self.generate_content_source(translated_summary, link)
+            articles.append({
+                'title': applyTextCorrections(translated_title),
+                'content': content,
+                'link': link,
+                'image': image_url
+            })
+        return articles
+
+    def get_article_content_and_image(self, url : str) -> str:
+        'Fetch the text from url and return it after parsing'
+        try:
+            html_content = getHtmlContent(url)
+        except ConnectionError:
+            return ""
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        article_text = ""
+        for element in soup.select("article p"):
+            article_text += "\n" + element.text
+
+        img_tags = soup.find_all('img')
+        return (article_text, img_tags)
+
+
+    def translate_article(self, text: str) -> str:
+        '''
+        To translate the texts from English to Portuguese
+        '''
+        try:
+            translated_content = translator.translate(text, src='en', dest='pt')
+        except TypeError:
+            logger.error("Translation failed for: %s", text)
+            return None
+        if translated_content.text == text:
+            logger.error("Translation failed: %s", translated_content.text)
+            return None
+        return translated_content.text
+
+    def generate_summary(self, article_text : str) -> str:
+        '''
+        Get the article and generate an automated summary.
+        '''
+        try:
+            sentences = sent_tokenize(article_text)
+        except LookupError:
+            logger.info("initializing nltk")
+            nltk.download('punkt')
+            nltk.download('stopwords')
+
+            # trying again
+            sentences = sent_tokenize(article_text)
+
+        stop_words = set(stopwords.words("english"))
+        word_frequencies = FreqDist()
+        for word in nltk.word_tokenize(article_text):
+            if word.lower() not in stop_words:
+                word_frequencies[word.lower()] += 1
+
+        sentence_scores = {}
+        for sentence in sentences:
+            for word in nltk.word_tokenize(sentence.lower()):
+                if word in word_frequencies.keys():
+                    if len(sentence.split(" ")) < 30:
+                        if sentence not in sentence_scores:
+                            sentence_scores[sentence] = word_frequencies[word]
+                        else:
+                            sentence_scores[sentence] += word_frequencies[word]
+
+        summary_sentences = nlargest(
+            5, sentence_scores, key=sentence_scores.get)
+        return " ".join(summary_sentences)
+
+    def is_summary_too_short(self, summary: str) -> bool:
+        'Is it shorter than 5 characters?'
+        return len(summary) < 5
+
+    def get_image_url_from_tag(self, title: str, image_tags_from_soap: list):
+        'Try to find a main image url'
+        if image_tags_from_soap is None:
+            logger.error("The [%s] has no image tags", title)
+            return None
+
+        tags_size = len(image_tags_from_soap)
+        if tags_size == 0:
+            logger.error("The [%s] has no image tags", title)
+            return None
+        try:
+            img_url = image_tags_from_soap[0]['src']
+            return img_url
+        except (KeyError, IndexError):
+            logger.error("Failed to find image for: [%s] - tags found: %d", title, tags_size)
+            return None
+
+    def generate_content_source(self, summary: str, link: str) -> str :
+        '''to generate the content to be posted with source information'''
+        return applyTextCorrections(summary) + \
+            "\n\nFonte: <a href=\"" + link + \
+            "\">" + link + "</a>"
+
 
 if __name__ == '__main__':
     import argparse
 
     parse = argparse.ArgumentParser(
         description='Automated Bot to Post into WordPress sites')
-    parse.add_argument('--config', help="configuration file")
+    parse.add_argument('--config', required=True, help="configuration file")
     parse.add_argument('--loglevel', help="logging level", default="DEBUG")
 
     args = parse.parse_args()
@@ -384,6 +442,8 @@ if __name__ == '__main__':
 
     if args.loglevel is not None:
         logger.setLevel(args.loglevel.upper())
+
+    get_interested_terms()
 
     logger.info('Starting at: %s', time.ctime())
     news = NewsBot(config=args.config)
